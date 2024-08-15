31ab334a1c5ae4e7156e8d8041957c65a3eeecc4
shin <nhathanh024@gmail.com>
2024-08-15 12:05:00 +0700

    fix issue missing font

--- src/wp-content/themes/zippy-child/includes/shin_custom.php
+++ src/wp-content/themes/zippy-child/includes/shin_custom.php
@@ -5,7 +5,7 @@ function shin_scripts()
 {
     $version = time();
 
-    wp_enqueue_style('main-style-css', THEME_URL . '-child' . '/dist/css/main.min.css', array(), $version, 'all');
+    wp_enqueue_style('main-style-css', THEME_URL . '-child' . '/assets/dist/css/main.min.css', array(), $version, 'all');
 
-    wp_enqueue_script('main-scripts-js', THEME_URL . '-child' . '/dist/js/main.min.js', array('jquery'), $version, true);
+    wp_enqueue_script('main-scripts-js', THEME_URL . '-child' . '/assets/dist/js/main.min.js', array('jquery'), $version, true);
 }
