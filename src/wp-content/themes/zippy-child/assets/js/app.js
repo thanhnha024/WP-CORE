d234ca3d34c5407f9beffbc98bed8b0748850751
shin <nhathanh024@gmail.com>
2024-10-28 18:01:39 +0700

    update wp version and woo version

--- src/wp-content/themes/zippy-child/assets/js/app.js
+++ src/wp-content/themes/zippy-child/assets/js/app.js
@@ -1,16 +1,16 @@
-// import { DisplayLabel } from './components/DisplayLabel';
+// // import { DisplayLabel } from './components/DisplayLabel';
 
-let Main = {
-  init: async function () {
+// let Main = {
+//   init: async function () {
 
-    // initialize demo javascript component - async/await invokes some 
-    //  level of babel transformation 
-    const displayLabel = new DisplayLabel();
-    await displayLabel.init();
+//     // initialize demo javascript component - async/await invokes some 
+//     //  level of babel transformation 
+//     const displayLabel = new DisplayLabel();
+//     await displayLabel.init();
 
-  }
-};
+//   }
+// };
 
 
-console.log($('.more-link'));
-// Main.init();
+// console.log($('.more-link'));
+// // Main.init();
